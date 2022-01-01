b230dc4263d29256a7354cb5e5a3d1670bd3b5c7
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2022-01-01 10:21:32 +0900

    lesson 9

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
+const path = require('path')
+
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
+  // `shared` でいいの？相対パス指定したほうが安全なんじゃ？
+  moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'shared'],
   moduleNameMapper: {
     '\\.module.css': 'identity-obj-proxy',
     '\\.css': require.resolve('./test/style-mock'),
---
+++ src/__tests__/calculator.js
@@ -0,0 +1,6 @@
+import {render} from '@testing-library/react'
+import Calculator from '../calculator'
+
+test('render', () => {
+  render(<Calculator />)
+})
