88f846f60a2efc379b5542ea5ba24b2c38927c1d
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2022-01-03 23:11:43 +0900

    lesson 14

--- jest.config.js
+++ jest.config.js
@@ -15,4 +15,5 @@ module.exports = {
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   snapshotSerializers: ['jest-emotion'],
+  collectCoverageFrom: ['**/src/**/*.js'],
 }
