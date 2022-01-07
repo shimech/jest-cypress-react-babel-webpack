2dec6d8f9fa386c5f1a4b5ed2f902324edb0cca7
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2022-01-07 22:31:55 +0900

    jest.config.js -> jest-common.js

--- test/jest-common.js
+++ test/jest-common.js
@@ -1,17 +1,18 @@
 const path = require('path')
 
 module.exports = {
+  rootDir: path.join(__dirname, '..'),
   testEnvironment: 'jest-environment-jsdom',
   // `shared` でいいの？相対パス指定したほうが安全なんじゃ？
   moduleDirectories: [
     'node_modules',
-    path.join(__dirname, 'src'),
+    path.join(__dirname, '../src'),
     'shared',
-    path.join(__dirname, 'test'),
+    __dirname,
   ],
   moduleNameMapper: {
     '\\.module.css': 'identity-obj-proxy',
-    '\\.css': require.resolve('./test/style-mock'),
+    '\\.css': require.resolve('./style-mock.js'),
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   snapshotSerializers: ['jest-emotion'],
