cd72097b26a132a487fca8ebcc15d803d4279483
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2022-01-07 22:55:15 +0900

    lesson 19

--- test/jest-common.js
+++ test/jest-common.js
@@ -2,7 +2,6 @@ const path = require('path')
 
 module.exports = {
   rootDir: path.join(__dirname, '..'),
-  testEnvironment: 'jest-environment-jsdom',
   // `shared` でいいの？相対パス指定したほうが安全なんじゃ？
   moduleDirectories: [
     'node_modules',
@@ -14,21 +13,5 @@ module.exports = {
     '\\.module.css': 'identity-obj-proxy',
     '\\.css': require.resolve('./style-mock.js'),
   },
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  snapshotSerializers: ['jest-emotion'],
   collectCoverageFrom: ['**/src/**/*.js'],
-  coverageThreshold: {
-    global: {
-      statements: 31,
-      branches: 18,
-      functions: 29,
-      lines: 29,
-    },
-    './src/shared/utils.js': {
-      statements: 100,
-      branches: 80,
-      functions: 100,
-      lines: 100,
-    },
-  },
 }
